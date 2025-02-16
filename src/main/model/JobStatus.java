ce1f199ed530e9c532bd013670221b077ed88475
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-02-15 20:41:32 -0800

    Implemented JobStatus class for test purposes

--- src/main/model/JobStatus.java
+++ src/main/model/JobStatus.java
@@ -2,5 +2,12 @@ package model;
 
 //Define available job application statuses
 public enum JobStatus {
-//stub 
+Applied, 
+First_interview,
+Second_interview,
+Third_interview,
+Final_interview,
+Offer_received,
+Withdrawn,
+Rejected;
 }
