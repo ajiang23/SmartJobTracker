12d2cd5019cc0f6c089a251264756d4799720700
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-02-15 23:17:35 -0800

    Implemented JobApplicationList class

--- src/main/model/JobApplicationList.java
+++ src/main/model/JobApplicationList.java
@@ -8,30 +8,50 @@ private ArrayList<JobApplication> newList;
 
 //Effects: instatiates an empty list of job applications
 public JobApplicationList(){
-    //stub
+    newList = new ArrayList<>();
 }
 
 //MODIFIES: this
-//EFFECTS: add a new job application to the list 
+//EFFECTS: add a new job application to the end of the list  
 public void addJob(JobApplication newJob){
-    //stub 
+    newList.add(newJob);
 }
 
 //REQUIRES: job application to be removed is part of the list 
 //MODIFIES: this 
 //EFFECTS: remove an existing job application from the list
 public void removeJob(JobApplication existingJob){
-    //stub 
+    newList.remove(existingJob);
 }
 
 //EFFECTS: filter current list by status
 public ArrayList<JobApplication> filterByStatus(JobStatus status){
-    return new ArrayList<>(); //stub
-}
+    ArrayList<JobApplication> filteredList = new ArrayList<>();
+
+    for(JobApplication nextJob:newList){
+        if (nextJob.getStatus() == status){
+            filteredList.add(nextJob);
+        }
+        }
+        return filteredList;
+    }
+
 
 // EFFECTS: filter current list by companyName
 public ArrayList<JobApplication> filterByCompany(String company) {
-    return new ArrayList<>(); // stub
+    ArrayList<JobApplication> filteredList = new ArrayList<>();
+
+    for (JobApplication nextJob : newList) {
+        if (nextJob.getCompanyName().equals(company)) {
+            filteredList.add(nextJob);
+        }
+    }
+    return filteredList;
+}
+
+//EFFECTS: return this job application list 
+public ArrayList<JobApplication> getList(){
+    return newList; 
 }
 
 }
