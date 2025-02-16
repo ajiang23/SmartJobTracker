f9a97d35ba40aae5c909e4c97d4bf089d2106df9
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-02-15 20:42:09 -0800

    Added tests for JobApplication class

---
+++ src/test/model/TestJobApplication.java
@@ -0,0 +1,35 @@
+package model;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.io.File;
+import java.time.LocalDate;
+import model.JobStatus;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class TestJobApplication {
+    private JobApplication jobApplication;
+
+    @BeforeEach
+    void runBefore() {
+        LocalDate appliedDate = LocalDate.of(2025,01,01);
+        File resume = new File ("resume.pdf");
+        String url = "https://www.sonypicturesjobs.com/search-jobs/audio/22978/1?glat=49.26539993286133&glon=-123.25499725341797";      
+
+        jobApplication = new JobApplication("Sony", "Digital Cinema Technician", appliedDate, resume, url);
+    }
+
+    @Test
+    void constructorTest() {
+        assertEquals("Sony", jobApplication.getCompanyName());
+        assertEquals("Digital Cinema Technician", jobApplication.getJobTitle());
+        assertEquals(LocalDate.of(2025, 01, 01), jobApplication.getAppliedDate());
+        assertEquals("resume.pdf", jobApplication.getResume());
+        assertEquals("https://www.sonypicturesjobs.com/search-jobs/audio/22978/1?glat=49.26539993286133&glon=-123.25499725341797", jobApplication.getPostingURL());
+        assertNull(jobApplication.getCoverLetter());
+        assertEquals(JobStatus.Applied,jobApplication.getStatus());
+        assertNull(jobApplication.getNotes());
+    }
+}
