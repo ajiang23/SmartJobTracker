ed5b46ddface639c7b0f2e9173560e123e392edf
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-02-22 14:53:18 -0800

    Fixed checkstyle issues

--- src/test/persistence/JsonWriterTest.java
+++ src/test/persistence/JsonWriterTest.java
@@ -10,19 +10,40 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.time.LocalDate;
+import org.junit.jupiter.api.BeforeEach;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class JsonWriterTest extends JsonTest {
+    JobApplication ja1;
+    JobApplication ja2;
+    File resume1;
+    File resume2;
+    File cl;
+
+    @BeforeEach
+    void constructrJL() {
+        resume1 = new File("src/test/resources/Test.pdf");
+        resume2 = new File("src/test/resources/Test1.pdf");
+        cl = new File("src/test/resources/Test2.pdf");
+        ja1 = new JobApplication("Amazon", "Developer", LocalDate.of(2024, 11, 05), resume1,
+                "amazon.ca");
+        ja1.setStatus(JobStatus.Final_interview);
+        ja2 = new JobApplication("Github", "Specialist", LocalDate.of(2025, 01, 07), resume2,
+                "github.ca");
+        ja2.setCoverLetter(cl);
+        ja2.setNotes("This is test note for ja2.");
+    }
 
     @Test
     void testWriterInvalidFile() {
         try {
-            JobApplicationList jl = new JobApplicationList();
-            JsonWriter writer = new JsonWriter("./data/invalidFile.json");
+            new JobApplicationList();
+            JsonWriter writer = new JsonWriter("./data/\0invalidFile.json");
             writer.open();
             fail("IOException expected.");
         } catch (FileNotFoundException pass) {
+            // pass
         }
     }
 
@@ -47,15 +68,7 @@ public class JsonWriterTest extends JsonTest {
     void testWriterGeneralJobAppList() {
         try {
             JobApplicationList jl = new JobApplicationList();
-            File resume1 = new File ("src/test/resources/Test.pdf");
-            File resume2 = new File ("src/test/resources/Test1.pdf");
-            File cl = new File ("src/test/resources/Test2.pdf");
-            JobApplication ja1 = new JobApplication ("Amazon", "Developer", LocalDate.of(2024, 11,05), resume1,  "amazon.ca"); 
-            ja1.setStatus(JobStatus.Final_interview);
             jl.addJob(ja1);
-            JobApplication ja2 = new JobApplication ("Github", "Specialist", LocalDate.of(2025,01,07), resume2, "github.ca");
-            ja2.setCoverLetter(cl);
-            ja2.setNotes("This is test note for ja2.");
             jl.addJob(ja2);
 
             JsonWriter writer = new JsonWriter("./data/testWriterGeneralJobAppList.json");
@@ -68,7 +81,8 @@ public class JsonWriterTest extends JsonTest {
             assertEquals(2, jl.getList().size());
             checkJobApplication("Amazon", "Developer", LocalDate.of(2024, 11, 05), resume1, "amazon.ca", null, "",
                     JobStatus.Final_interview, jl.getList().get(0));
-            checkJobApplication("Github", "Specialist", LocalDate.of(2025, 01, 07), resume2, "github.ca", cl, "This is test note for ja2.",
+            checkJobApplication("Github", "Specialist", LocalDate.of(2025, 01, 07), resume2, "github.ca", cl,
+                    "This is test note for ja2.",
                     JobStatus.Applied, jl.getList().get(1));
         } catch (IOException e) {
             fail("File writing error.");
