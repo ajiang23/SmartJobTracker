1ff490af95b478bdd707561b18a7304ac829ad44
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-02-22 13:44:01 -0800

    implemented JsonWriter

--- src/main/persistence/JsonWriter.java
+++ src/main/persistence/JsonWriter.java
@@ -1,7 +1,10 @@
 package persistence;
 
+import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import model.JobApplicationList;
+import java.io.File;
+import org.json.JSONObject;
 
 //Represents a writer writing JSON representation of job applications to file
 public class JsonWriter {
@@ -11,30 +14,31 @@ public class JsonWriter {
     
     //EFFECTS: constructs writer to write to destination file
     public JsonWriter(String destination) {
-        //stub
+        this.destination = destination;
     }
 
     //MODIFIES: this
     //EFFECTS: open writer and prints errors if file fails to open
-    public void open(){
-        //stub
+    public void open() throws FileNotFoundException {
+        writer = new PrintWriter (new File(destination));
     }
 
     //MODIFIES: this 
     //EFFECTS: writes JSON representation of job application list to file
     public void write(JobApplicationList jobList){
-        //stub
+        JSONObject json = jobList.toJson();
+        saveToFile(json.toString(TAB)); 
     }
 
     //MODIFIES: this
     //EFFECTS: writes string to file and ensures it's saved
     public void saveToFile(String json) {
-        //stub
+        writer.print(json);
     }
 
     //MODIFIES: this
     //EFFECTS: closes writer safely 
     public void close(){
-        //stub
+        writer.close();
     }
 }
