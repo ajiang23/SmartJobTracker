1e1e5bafcc481305acc4a444f1c1d369af4d396d
Alicia Jiang <l2e6x@ugrad.cs.ubc.ca>
2025-03-21 15:03:57 -0700

    Fixed bugs

--- src/main/ui/Main.java
+++ src/main/ui/Main.java
@@ -1,13 +1,21 @@
 package ui;
 
-import java.io.FileNotFoundException;
+// import java.io.FileNotFoundException;
+import javax.swing.SwingUtilities;
+
+// public class Main {
+//     public static void main(String[] args) {
+//         try {
+//             new JobTrackingApp();
+//         } catch (FileNotFoundException e) {
+//             System.out.println("Unable to run app: file not found.");
+//         }
+//     }
+// }
+
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            new JobTrackingApp();
-        } catch (FileNotFoundException e) {
-            System.out.println("Unable to run app: file not found.");
-        }
+        SwingUtilities.invokeLater(JobTrackingGUI::new); // ✅ Launch GUI
     }
 }
